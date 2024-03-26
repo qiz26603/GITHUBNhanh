2fe6690a3f5d22c1593d0e5e7d66f825eff5ad86
quyet <quyet26062003@gmail.com>
2024-03-26 16:39:44 +0700

    Update bcc5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Branch Preventive to do something!");
         System.out.printf("Hello and welcome To My New Branch!");
+        System.out.println("Bảng cửu chương 5: ");
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Hallo lần thứ " + (i+1));
+            System.out.println("5 x " + i + " = " + (5*i));
         }
     }
 }
