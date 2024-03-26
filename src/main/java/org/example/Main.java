dbc548cc4af38313f7082b375067c15b13acc6b9
Kerito <61581776+qiz26603@users.noreply.github.com>
2024-03-26 15:37:26 +0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.printf("Branch Preventive to do something!");
         System.out.printf("Hello and welcome To My New Branch!");
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hallo lần thứ " + (i+1));
