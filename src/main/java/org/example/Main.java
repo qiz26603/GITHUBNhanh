d4f03815d8493894f5977c22cd2c98beb74c6ad4
Phanphung2604 <minhphungphan26403@gmail.com>
2024-03-26 17:06:26 +0700

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.printf("Hello and welcome To My New Branch!");
+        System.out.println("Bang cuu chuong 3");
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Hallo lần thứ " + (i+1));
+            System.out.println(" 3 * " i " = " + 3 * i);
         }
     }
 }
