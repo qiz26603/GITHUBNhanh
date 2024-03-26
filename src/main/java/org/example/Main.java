39bd99a11caa2e5c3ad2382b59d6c658ca3b8666
quyet <quyet26062003@gmail.com>
2024-03-26 15:51:39 +0700

    First Edition

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.printf("Hello and welcome To My New Branch!");
+        System.out.printf("Hello and welcome To My Master Branch!");
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hallo lần thứ " + (i+1));
         }
