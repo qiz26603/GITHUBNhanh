7aa657c749b80308db0634e2766ab933a418f13a
quyet <quyet26062003@gmail.com>
2024-03-27 09:49:26 +0700

    edit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.printf("Hello and welcome To My Master Branch!");
-        System.out.println("Bảng cửu chương 5: ");
+        System.out.println("Bang cuu chuong 3");
         for (int i = 1; i <= 10; i++) {
-            System.out.println("5 x " + i + " = " + (5 * i));
+            System.out.println(" 3 * " + i + " = " + 3 * i);
         }
     }
 }
